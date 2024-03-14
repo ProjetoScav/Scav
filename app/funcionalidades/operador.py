ff419a7c5b51b08db8ac85bc6f6916fef12ba32b
Thomas Passos <tm123br@live.com>
2024-03-14 15:59:26 -0300

    Avanços do frontend

--- app/funcionalidades/operador.py
+++ app/funcionalidades/operador.py
@@ -1,15 +1,15 @@
 import math
 
 from app.conectores.conectores import ApiCnpjLigação, ApiExtendidaLigação
-from objetos import (
+from app.modelos.objetos import (
     GeradorDeObjetos,
     MudançaExtras,
     MudançaQuery,
     MudançaRangeQuery,
     Requisição,
 )
-from processadores import ProcessadorDeDados
 
+from .processadores import ProcessadorDeDados
 
 class Operador:
     '''Classe central da aplicação, responsável pelo manejo 
@@ -20,14 +20,15 @@ class Operador:
         self.processador: ProcessadorDeDados = ProcessadorDeDados()
         self.gerador: GeradorDeObjetos = GeradorDeObjetos()
         self.requisição: Requisição = self.gerador.gerar_requisição()
-        self.requisições = []
+        self.requisições: list[Requisição] = []
         self.cnpjs: list = []
         self.paginas: list = []
+        self.modificações: list[MudançaRangeQuery | MudançaExtras | MudançaQuery] = []
 
-    def modificar_requisição(self, modificações: list[MudançaRangeQuery | MudançaExtras | MudançaQuery]):
+    def modificar_requisição(self):
         '''Função que faz as alterações na requisição 
         através de uma lista de objetos de mudança'''
-        for modificação in modificações:
+        for modificação in self.modificações:
             if isinstance(modificação, MudançaQuery):
                 self.requisição.mudar_query(modificação)
             elif isinstance(modificação, MudançaRangeQuery):
@@ -43,7 +44,7 @@ class Operador:
         self.requisições.append(self.requisição)
         self.requisição = self.gerador.gerar_requisição()
     
-    def pegar_numero_paginas(self, requisição: Requisição) -> int:
+    def pegar_numero_paginas_cnpjs(self, requisição: Requisição) -> int:
         '''Função que recebe uma requisição da Casa dos Dados
         e calcula sua quantidade de páginas'''
         resposta = self.conector_extendida.fazer_a_requisição(requisição.gerar_json())
@@ -56,13 +57,13 @@ class Operador:
         else: 
             n_paginas = 1
         
-        return n_paginas
+        return n_paginas, n_dados
 
     def fazer_requisições_cnpj(self):
         '''Função que faz a requisição na API da Casa de Dados
         e salvas os cnpjs'''
         for requisição in self.requisições:
-            numero_paginas = self.pegar_numero_paginas(requisição)
+            numero_paginas, n_dados = self.pegar_numero_paginas_cnpjs(requisição)
             for i in range(1, numero_paginas + 1):
                 json = requisição.gerar_json(i)
                 resposta = self.conector_extendida.fazer_a_requisição(json)
@@ -84,7 +85,41 @@ class Operador:
         self.fazer_requisições_dados()
 
     def exportar_os_dados(self):
+        self.puxar_dados()
         '''Função que exporta os dados em um arquivo .xlxs'''
         linhas = self.processador.scrape_dos_dados(self.paginas)
         df = self.processador.criar_dataframe(linhas)
         self.processador.exportar_dataframe(df)
+
+
+class GarçomDoFront:
+    def __init__(self, requisição: Requisição):
+        self.requisição = requisição
+        self.numero_paginas_api, self.numero_cnpjs = Operador().pegar_numero_paginas_cnpjs(requisição)
+        self.numero_paginas_tela: int = self.numero_de_paginas_tela()
+    
+    def numero_de_paginas_tela(self) -> int:
+        if self.numero_cnpjs > 100:
+           return 10
+        elif self.numero_cnpjs < 10:
+            return 1
+        else:
+            return math.ceil(self.numero_cnpjs / 10)
+        
+    def gerar_dados_cards(self):
+        cnpjs = []
+        for i in range(1 , math.ceil(self.numero_paginas_tela / 2) + 1):
+            json = self.requisição.gerar_json(i)
+            resposta = ApiExtendidaLigação().fazer_a_requisição(json)
+            json = resposta.json()
+            dados = json["data"]["cnpj"]
+            for dado in dados:
+                cnpj, razao, municipio, estado, cadastro = ProcessadorDeDados().pegar_dados_frontend(dado) 
+                cnpj_objeto = GeradorDeObjetos().gerar_campo_de_dados(
+                                                                    razao=razao, 
+                                                                    cnpj=cnpj, 
+                                                                    cadastro=cadastro,
+                                                                    municipio=municipio,
+                                                                    estado=estado)
+                cnpjs.append(cnpj_objeto)
+        return cnpjs
--- app/rotas/rotas.py
+++ app/rotas/rotas.py
@@ -1,7 +1,25 @@
 from flask.templating import render_template
 from flask import request
+from app.funcionalidades.operador import Operador, GarçomDoFront
 
 def configure(app):
     @app.route("/")
     def home():
-        return render_template("index.html")
+        pagina = request.args.get('pagina', 1, type=int)
+        operador = Operador()
+        front = GarçomDoFront(operador.requisição)
+        n_paginas = front.numero_paginas_tela
+        fim_cards = pagina * 10
+        começo_cards = fim_cards - 10
+        cards = front.gerar_dados_cards()
+        cards = cards[começo_cards: fim_cards]
+        return render_template("index.html", 
+                               cards=cards, 
+                               n_de_dados=front.numero_cnpjs,
+                               n_paginas=n_paginas,
+                               pagina=pagina)
+
+
+    @app.route("/cnpj/<int:cnpj>")
+    def cnpj(cnpj):
+        return render_template("cnpj.html")
