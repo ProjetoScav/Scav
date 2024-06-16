74921aacfe2433f9557ce87bae585715c151605e
Thomas Passos <tm123br@live.com>
2024-06-16 18:59:21 -0300

    refatoração: substituição de código por imagens .svg e criação de templates

---
+++ app/obj/data/cnpj.py
@@ -0,0 +1,33 @@
+from dataclasses import asdict, dataclass
+from typing import Optional
+
+from app.ext.db.models import Estabelecimento
+
+
+@dataclass
+class CNPJ:
+    estabelecimento: Estabelecimento
+
+    def extrair_propriedades(self):
+        self.__setattr__()
+        ...
+
+    def transformar_em_dicionario(self) -> dict[str, str]:
+        "Método que transforma a classe em um dicionário"
+        return {k: str(v) for k, v in asdict(self).items()}
+
+    def __lista_to_string(self, lista: list) -> Optional[str]:
+        """Método que converte uma lista em uma string pra planilha"""
+        try:
+            lista = [f"{item}," for item in lista if item]
+            return "".join(lista)[:-1]
+        except Exception:
+            return None
+
+    def ajustar_dados_pra_download(self) -> Optional[str]:
+        "Método que ajusta os dados pra inserção na planilha"
+        cnpj = self.transformar_em_dicionario()
+        for chave in ["telefones", "quadro_societario", "atividades_secundarias"]:
+            if cnpj[chave]:
+                cnpj[chave] = self.__lista_to_string(cnpj[chave])
+        return cnpj
---
+++ app/obj/data/mappers.py
@@ -0,0 +1,11 @@
+situacao_cadastral: dict[int, str] = {
+    1: "Nula",
+    2: "Ativa",
+    3: "Suspensa",
+    4: "Inapta",
+    8: "Baixada",
+}
+
+matriz_filial: dict[int, str] = {1: "Matriz", 2: "Filial"}
+
+mei: dict[str, str] = {"S": "Sim", "N": "Não", "EM BRANCO": "Outros"}
